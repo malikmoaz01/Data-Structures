fcc19be0664ecb626910cf56c24acfb79ff2277d
mlkmoaz01 <mlkmoaz01@gmail.com>
2024-08-15 19:35:38 +0500

    CSES Solutions Updated

--- CSES Problems Solution/1. Introductory problems/6. Number Spiral.cpp
+++ CSES Problems Solution/1. Introductory problems/6. Number Spiral.cpp
@@ -6,20 +6,17 @@ int main() {
     cin>>t;
     while(t--)
     {
-	int arr[3][4] = {
+	int arr[5][5] = {
         {1, 2, 9 , 10 , 25},
         {4 , 3 , 8 , 11 , 24},
         {5 , 6 , 7 , 12 , 23},
         {16 , 15 , 14 , 13 , 22},
-        {17 , 18 , 19 , 20 , 21};
-    };
-    	int rows;
-    	cin>>rows;
-    	int cols;
-    	cin>>cols;
-    	
-    	
-    	
-    	
+        {17 , 18 , 19 , 20 , 21} };
+    	int rows , cols;
+    	cin>>rows>>cols;
+    	int index_row = rows - 1;
+        int index_cols = cols - 1;
+    	cout<<arr[index_row][index_cols];
 	}
+		return 0;
 }
