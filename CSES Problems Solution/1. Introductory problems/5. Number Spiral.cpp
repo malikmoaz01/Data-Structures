1f163fb5b87c3e4a3940512de349031e64ac00a2
mlkmoaz01 <mlkmoaz01@gmail.com>
2024-08-15 19:13:19 +0500

    CSES Solutions Updated

--- CSES Problems Solution/1. Introductory problems/5. Number Spiral.cpp
+++ CSES Problems Solution/1. Introductory problems/5. Number Spiral.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
-    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	int arr[3][4] = {
+        {1, 2, 9 , 10 , 25},
+        {4 , 3 , 8 , 11 , 24},
+        {5 , 6 , 7 , 12 , 23},
+        {16 , 15 , 14 , 13 , 22},
+        {17 , 18 , 19 , 20 , 21};
+    };
+    	int rows;
+    	cin>>rows;
+    	int cols;
+    	cin>>cols;
+    	
+    	
+    	
+    	
+	}
 }
