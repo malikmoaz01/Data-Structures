c1bc65d4031cc9fcf3c82d758d2c658a108b38a3
mlkmoaz01 <mlkmoaz01@gmail.com>
2024-08-16 23:48:50 +0500

    DSA Sheet Updated

--- DSA Sheet/Arrays/Searching & Sorting/Permute_arrs_pair_greater_k.cpp
+++ DSA Sheet/Arrays/Searching & Sorting/Permute_arrs_pair_greater_k.cpp
@@ -1,32 +1,43 @@
 #include <bits/stdc++.h>
-using namespace std ;
-int main()
-{
-    int n ;
-    vector<int> arr(n);
-    int n1;
-    vector<int> arr1(n1);
 
-    cout << "Enter array 1 elements :  ";
-    for(int i=0; i<n; i++)
-    {
-        
-        cin>>arr[i];
-    }
+using namespace std;
 
-    cout << "Enter array 2 elements :  ";
-    for(int i=0; i<n1; i++)
-    {
-        cin>>arr1[i];
-    }
-
-    sort(arr.begin() , arr.end());
-    sort(arr1.begin() , arr1.end() , greater<int>);
-
-    int k ;
-    cout<<"Enter k value ";
-    cin>>k ;
+int main() {
+    int n;
+    cin >> n;
     
-    for(int i=0 ;)
+    vector<int> arr(n);
+    vector<int> arr1(n);
+    
+    cout << "Enter array 1 elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
+    cout << "Enter array 2 elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr1[i];
+    }
+    
+    int k;
+    cout << "Enter k value: ";
+    cin >> k;
+
+    sort(arr.begin(), arr.end());
+    sort(arr1.begin(), arr1.end(), greater<int>());
+    bool possible = true;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] + arr1[i] < k) {
+            possible = false;
+            break;
+        }
+    }
+
+    if (possible) {
+        cout << "Yes\n";
+    } else {
+        cout << "No\n";
+    }
+    
+    return 0;
 }
