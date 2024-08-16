b4f6c8da58c8025a2071076033860e593a9ad4a2
mlkmoaz01 <mlkmoaz01@gmail.com>
2024-08-16 23:19:22 +0500

    DSA Sheet Updated

--- DSA Sheet/Strings/Convert_sentence_into_mobile_no.cpp
+++ DSA Sheet/Strings/Convert_sentence_into_mobile_no.cpp
@@ -2,7 +2,23 @@
 using namespace std ;
 int main()
 {
-    string s;
-    cin>>s;
+    string input;
+    cin>>input;
+    string str[] = { "2", "22", "222", "3", "33", "333", "4",
+            "44", "444", "5", "55", "555", "6", "66",
+            "666", "7", "77", "777", "7777", "8", "88",
+            "888", "9", "99", "999", "9999" };
     
+    string output ;
+    for(int i=0; i<input.size(); i++)
+    {
+        if(input == " ")
+        {
+            output += "0";
+        }else{
+            int position = input[i] - 'A' ;
+            output += str[position];
+        }
+    }
+    cout<<output;
 }
