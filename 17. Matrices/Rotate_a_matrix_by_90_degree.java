ce1f2b1901403c2df054ed6b80d8c7c7a00ed273
mlkmoaz01 <mlkmoaz01@gmail.com>
2024-08-21 00:14:48 +0500

    Matrices Problems'

---
+++ 17. Matrices/Rotate_a_matrix_by_90_degree.java
@@ -0,0 +1,44 @@
+public class MatrixTransposeAndReverse {
+
+    public static void transposeAndReverseColumns(int[][] matrix) {
+        int n = matrix.length;
+        
+        // Transpose 
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        // Reverse each column
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n / 2; i++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int elem : row) {
+                System.out.print(elem + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        transposeAndReverseColumns(matrix);
+        printMatrix(matrix);
+    }
+}
