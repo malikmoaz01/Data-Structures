f409272f5a5b97a32fbf9353853d9709b08ebf10
mlkmoaz01 <mlkmoaz01@gmail.com>
2024-08-22 22:13:50 +0500

    Array Problems

---
+++ 1. Arrays/Gfg Medium/Count_SubArray_0_1.java
@@ -0,0 +1,33 @@
+public class Equal01Subarrays {
+
+    public static int countSubarrays(int[] arr) {
+        int n = arr.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int zeros = 0;
+            int ones = 0;
+
+            for (int j = i; j < n; j++) {
+                if (arr[j] == 0) {
+                    zeros++;
+                } else {
+                    ones++;
+                }
+
+                if (zeros == ones) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 0, 0, 1, 0, 1, 1};
+        int[] arr2 = {1, 0, 0, 1, 1, 0, 0, 1};
+
+        System.out.println(countSubarrays(arr1));  
+    }
+}
