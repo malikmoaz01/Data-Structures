c5403a98a6b8d0f81045906183f15e13aece558a
mlkmoaz01 <mlkmoaz01@gmail.com>
2024-08-22 22:46:42 +0500

    Array Medium

---
+++ 1. Arrays/Gfg Medium/Factorial_of_Large_Number.java
@@ -0,0 +1,26 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class LargeFactorial {
+
+    public static BigInteger factorial(int n) {
+        BigInteger result = BigInteger.ONE; 
+
+        for (int i = 2; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        BigInteger fact = factorial(n);
+        System.out.println("Factorial of " + n + " is: " + fact);
+        
+        scanner.close();
+    }
+}
---
+++ 1. Arrays/Kadanes Algo.java
@@ -0,0 +1,22 @@
+public class MaxSubArraySum {
+
+    public static int maxSubArraySum(int[] a) {
+        int max_so_far = Integer.MIN_VALUE;
+        int max_ending_here = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            max_ending_here += a[i];
+
+            max_so_far = Math.max(max_so_far, max_ending_here);
+
+            max_ending_here = Math.max(max_ending_here, 0);
+        }
+
+        return max_so_far;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {-2, -3, 4, -1, -2, 1, 5, -3};
+        System.out.println("Maximum subarray sum is " + maxSubArraySum(a));
+    }
+}
