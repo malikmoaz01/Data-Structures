153704b46149692e7565a3279a41f2e123b99edf
Malik Moaz <116621442+malikmoaz01@users.noreply.github.com>
2024-08-23 20:50:50 +0500

    Update 296. Flood_Fill.java

--- DSA Sheet/Graphs/296. Flood_Fill.java
+++ DSA Sheet/Graphs/296. Flood_Fill.java
@@ -10,14 +10,13 @@ class Solution {
         int cols = grid[0].length;
         int startColor = grid[startrow][startcol];
         
-        // If the new color is the same as the start color, no need to do anything.
         if (startColor == color) {
             return grid;
         }
         
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{startrow, startcol});
-        grid[startrow][startcol] = color; // Color the start cell
+        grid[startrow][startcol] = color; 
 
         while (!q.isEmpty()) {
             int[] current = q.poll();
@@ -27,11 +26,10 @@ class Solution {
             for (int i = 0; i < 4; i++) {
                 int r = drows[i] + row;
                 int c = dcols[i] + col;
-
-                // Check if the adjacent cell is within bounds and has the start color
+                
                 if (r >= 0 && r < rows && c >= 0 && c < cols && grid[r][c] == startColor) {
                     q.add(new int[]{r, c});
-                    grid[r][c] = color; // Color the adjacent cell
+                    grid[r][c] = color; 
                 }
             }
         }
@@ -46,13 +44,11 @@ class Solution {
             {1, 1, 0},
             {1, 0, 1}
         };
-        int sr = 1; // Start row
-        int sc = 1; // Start column
+        int sr = 1; 
+        int sc = 1; 
         int newColor = 2;
         
         int[][] result = solution.floodFill(image, sr, sc, newColor);
-        
-        // Print the result
         for (int[] row : result) {
             for (int pixel : row) {
                 System.out.print(pixel + " ");
