0c7d1b5f72112cc2879bda07ee9bd48267973c0a
mlkmoaz01 <mlkmoaz01@gmail.com>
2024-12-24 23:47:14 +0500

    LeetCode Greedy Problems

---
+++ 14. Greedy/LeetCode Problems/BuyTwoChocolates.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class BuyTwoChocolates {
+    
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+
+        if (prices.length < 2) {
+            return money;
+        }
+
+        int totalCost = prices[0] + prices[1];
+
+        if (money >= totalCost) {
+            return money - totalCost;
+        } else {
+            return money;
+        }
+    }
+
+    public static void main(String[] args) {
+        BuyTwoChocolates obj = new BuyTwoChocolates();
+        int[] prices = {5, 3, 8, 6};
+        int money = 10;
+        
+        int remainingMoney = obj.buyChoco(prices, money);
+        System.out.println("Remaining money: " + remainingMoney);
+    }
+}
