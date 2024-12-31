cef7763fe24d7aae5ab3d4bb2510e897a514d8d6
mlkmoaz01 <mlkmoaz01@gmail.com>
2024-12-31 21:41:07 +0500

    Dp KP

---
+++ 15. Dp/Knapsack_Tabulation.java
@@ -0,0 +1,33 @@
+public class Knapsack_Tabulation {
+
+    public static int knapSack(int W, int[] weights, int[] values, int n) {
+        int[][] dp = new int[n + 1][W + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                } else if (weights[i - 1] <= w) {
+                    dp[i][w] = Math.max(
+                        values[i - 1] + dp[i - 1][w - weights[i - 1]],
+                        dp[i - 1][w]
+                    );
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+
+        return dp[n][W];
+    }
+
+    public static void main(String[] args) {
+        int[] values = {60, 100, 120};
+        int[] weights = {10, 20, 30};
+        int W = 50;
+        int n = values.length;
+
+        int maxValue = knapSack(W, weights, values, n);
+        System.out.println("Maximum value in knapsack: " + maxValue);
+    }
+}
